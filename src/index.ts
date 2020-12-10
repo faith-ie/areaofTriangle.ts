2a7e3dafbdb0dbc5f53cac5ade120e88bc43a3d9
Faith1sGay <daniel.viola001@mymdc.net>
2020-12-09 19:05:25 -0500

    a

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { triangle } from './triangle'
+const tri = new triangle()
+console.log(`The area of this triangle is ${tri.height * tri.width / 2}.`)
---
+++ src/triangle.ts
@@ -0,0 +1,8 @@
+export class triangle {
+height : number
+width : number
+constructor(){
+    this.height = this.height
+    this.width = this.width
+}
+}
