8600eec5d58312593be46aa76f062d6704c6b293
Faith1sGay <daniel.viola001@mymdc.net>
2021-05-12 17:09:20 -0400

    finally fixed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { triangle } from './triangle'
-const tri = new triangle()
+import { Triangle } from './triangle'
+const tri = new Triangle(1, 4)
 console.log(`The area of this triangle is ${tri.height * tri.width / 2}.`)
--- src/triangle.ts
+++ src/triangle.ts
@@ -1,8 +1,8 @@
-export class triangle {
-height : number
-width : number
-constructor(){
-    this.height = this.height
-    this.width = this.width
-}
-}
+
+export class Triangle {
+    height : number
+    width : number
+constructor(height : number, width : number ){
+    this.height = height
+    this.width = width
+}}
